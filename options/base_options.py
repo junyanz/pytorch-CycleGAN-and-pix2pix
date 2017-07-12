23eb65de64e62141258acc258c930c6a63daaf42
junyanz <junyanz@berkeley.edu>
2017-07-11 21:38:14 -0700

    fix the bug when set gpu_ids=1

--- options/base_options.py
+++ options/base_options.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 from util import util
-
+import torch
 
 class BaseOptions():
     def __init__(self):
@@ -54,6 +54,10 @@ class BaseOptions():
             id = int(str_id)
             if id >= 0:
                 self.opt.gpu_ids.append(id)
+        
+        # set gpu ids
+        if len(self.opt.gpu_ids) > 0:
+            torch.cuda.set_device(self.opt.gpu_ids[0])
 
         args = vars(self.opt)
 
