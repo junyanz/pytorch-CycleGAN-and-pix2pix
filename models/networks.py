7bda734dd7f3466d5d55afe80b97542b1b12bdb5
Taesung Park <taesung89@gmail.com>
2017-12-01 22:13:51 -0800

    changed the gain of xavier initialization from 1 to 0.02. implemented serial_batches option in unaligned dataset

--- models/networks.py
+++ models/networks.py
@@ -26,9 +26,9 @@ def weights_init_xavier(m):
     classname = m.__class__.__name__
     # print(classname)
     if classname.find('Conv') != -1:
-        init.xavier_normal(m.weight.data, gain=1)
+        init.xavier_normal(m.weight.data, gain=0.02)
     elif classname.find('Linear') != -1:
-        init.xavier_normal(m.weight.data, gain=1)
+        init.xavier_normal(m.weight.data, gain=0.02)
     elif classname.find('BatchNorm2d') != -1:
         init.normal(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
