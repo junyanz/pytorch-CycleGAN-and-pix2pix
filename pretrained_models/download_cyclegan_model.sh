2dca893addf2cefe555e816dbc2565f490f8bb06
Taesung <taesung89@gmail.com>
2018-07-09 20:04:26 -0700

    added pretrained models of cyclegan

--- pretrained_models/download_cyclegan_model.sh
+++ pretrained_models/download_cyclegan_model.sh
@@ -1,12 +1,12 @@
 FILE=$1
 
-echo "Note: available models are horse2zebra, zebra2horse"
+echo "Note: available models are apple2orange, orange2apple, summer2winter_yosemite, winter2summer_yosemite, horse2zebra, zebra2horse, monet2photo, style_monet, style_cezanne, style_ukiyoe, style_vangogh, sat2map, map2sat, cityscapes_photo2label, cityscapes_label2photo, facades_photo2label, facades_label2photo, iphone2dslr_flower"
 
 echo "Specified [$FILE]"
 
 mkdir -p ./checkpoints/${FILE}_pretrained
 MODEL_FILE=./checkpoints/${FILE}_pretrained/latest_net_G.pth
-URL=https://people.eecs.berkeley.edu/~taesung_park/pytorch-CycleGAN-and-pix2pix/models/$FILE.pth
+URL=http://efrosgans.eecs.berkeley.edu/cyclegan/pretrained_models/$FILE.pth
 
 wget -N $URL -O $MODEL_FILE
 
