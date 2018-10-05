a1c237c4d92747fc9649c84ad81a5dc0b05c171b
junyanz <junyanzhu89@gmail.com>
2018-10-05 14:40:17 -0400

    update combine_A_and_B

--- datasets/combine_A_and_B.py
+++ datasets/combine_A_and_B.py
@@ -7,12 +7,12 @@ parser = argparse.ArgumentParser('create image pairs')
 parser.add_argument('--fold_A', dest='fold_A', help='input directory for image A', type=str, default='../dataset/50kshoes_edges')
 parser.add_argument('--fold_B', dest='fold_B', help='input directory for image B', type=str, default='../dataset/50kshoes_jpg')
 parser.add_argument('--fold_AB', dest='fold_AB', help='output directory', type=str, default='../dataset/test_AB')
-parser.add_argument('--num_imgs', dest='num_imgs', help='number of images',type=int, default=1000000)
-parser.add_argument('--use_AB', dest='use_AB', help='if true: (0001_A, 0001_B) to (0001_AB)',action='store_true')
+parser.add_argument('--num_imgs', dest='num_imgs', help='number of images', type=int, default=1000000)
+parser.add_argument('--use_AB', dest='use_AB', help='if true: (0001_A, 0001_B) to (0001_AB)', action='store_true')
 args = parser.parse_args()
 
 for arg in vars(args):
-    print('[%s] = ' % arg,  getattr(args, arg))
+    print('[%s] = ' % arg, getattr(args, arg))
 
 splits = os.listdir(args.fold_A)
 
@@ -40,7 +40,7 @@ for sp in splits:
         if os.path.isfile(path_A) and os.path.isfile(path_B):
             name_AB = name_A
             if args.use_AB:
-                name_AB = name_AB.replace('_A.', '.') # remove _A
+                name_AB = name_AB.replace('_A.', '.')  # remove _A
             path_AB = os.path.join(img_fold_AB, name_AB)
             im_A = cv2.imread(path_A, cv2.CV_LOAD_IMAGE_COLOR)
             im_B = cv2.imread(path_B, cv2.CV_LOAD_IMAGE_COLOR)
