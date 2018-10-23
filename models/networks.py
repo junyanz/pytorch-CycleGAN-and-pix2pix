bc2917e928255e4d1b3740fc88413068f4cd10c7
Jun-Yan Zhu <junyanz@users.noreply.github.com>
2018-10-23 17:23:59 -0400

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -24,7 +24,7 @@ def get_norm_layer(norm_type='instance'):
 def get_scheduler(optimizer, opt):
     if opt.lr_policy == 'lambda':
         def lambda_rule(epoch):
-            lr_l = 1.0 - max(0, epoch + 1 + opt.epoch_count - opt.niter) / float(opt.niter_decay + 1)
+            lr_l = 1.0 - max(0, epoch + opt.epoch_count - opt.niter) / float(opt.niter_decay + 1)
             return lr_l
         scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
     elif opt.lr_policy == 'step':
