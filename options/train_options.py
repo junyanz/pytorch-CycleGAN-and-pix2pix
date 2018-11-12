4ab494a6b4185f533d67ba304bdd59a878158d52
Hungryof <ziquanlanyue@163.com>
2018-11-12 10:09:27 +0800

    delete save_iter_freq

--- options/train_options.py
+++ options/train_options.py
@@ -15,7 +15,6 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
         parser.add_argument('--save_epoch_freq', type=int, default=5, help='frequency of saving checkpoints at the end of epochs')
         parser.add_argument('--save_by_iter', action='store_true', help='whether saves model by iteration')
-        parser.add_argument('--save_iter_freq', type=int, default=100, help='frequency of saving checkpoints at the end of iterations')
         parser.add_argument('--continue_train', action='store_true', help='continue training: load the latest model')
         parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
