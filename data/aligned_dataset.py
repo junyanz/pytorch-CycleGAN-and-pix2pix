22fe4465c120ac92a2de09c3c2d50cbbe0e63f29
junyanz <junyanzhu89@gmail.com>
2019-04-14 18:00:41 -0400

    update data loader

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -1,7 +1,5 @@
 import os.path
-import random
 from data.base_dataset import BaseDataset, get_params, get_transform
-import torchvision.transforms as transforms
 from data.image_folder import make_dataset
 from PIL import Image
 
--- data/base_dataset.py
+++ data/base_dataset.py
@@ -104,9 +104,11 @@ def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, conve
             transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
 
     if convert:
-        transform_list += [transforms.ToTensor(),
-                           transforms.Normalize((0.5, 0.5, 0.5),
-                                                (0.5, 0.5, 0.5))]
+        transform_list += [transforms.ToTensor()]
+        if grayscale:
+            transform_list += [transforms.Normalize((0.5,), (0.5,))]
+        else:
+            transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     return transforms.Compose(transform_list)
 
 
