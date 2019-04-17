f707b6293c7896273d2a7a6d4a069b670d4a3fc7
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2019-04-17 08:47:36 +0800

    minor update

--- models/test_model.py
+++ models/test_model.py
@@ -42,8 +42,8 @@ class TestModel(BaseModel):
         self.visual_names = ['real_A', 'fake_B']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         self.model_names = ['G' + opt.model_suffix]  # only generator is needed.
-        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG,
-                                      opt.norm, not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm, opt.use_spectral_norm_G,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
 
         # assigns the model to self.netG_[suffix] so that it can be loaded
         # please see <BaseModel.load_networks>
