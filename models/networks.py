8a6175415e57ce61e2ba35ff93507d7d54366430
Jun-Yan Zhu <junyanz@users.noreply.github.com>
2019-07-08 23:11:41 -0400

    Fix a typo suggested by @JingWang18

--- models/networks.py
+++ models/networks.py
@@ -199,7 +199,7 @@ def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal'
     elif netD == 'pixel':     # classify if each pixel is real or fake
         net = PixelDiscriminator(input_nc, ndf, norm_layer=norm_layer)
     else:
-        raise NotImplementedError('Discriminator model name [%s] is not recognized' % net)
+        raise NotImplementedError('Discriminator model name [%s] is not recognized' % netD)
     return init_net(net, init_type, init_gain, gpu_ids)
 
 
