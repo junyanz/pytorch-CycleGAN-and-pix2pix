c04fd2059848c66f690f194d5b215d2641a093d7
bossemel <18574762+bossemel@users.noreply.github.com>
2020-02-19 18:01:17 +0000

    input_nc

--- scripts/pix2pix_mnist_4channel.sh
+++ scripts/pix2pix_mnist_4channel.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/mnist_4channel/AB --name mnist_4channel_pix2pix_bs8 --model pix2pix --direction AtoB
+python test.py --dataroot ./datasets/mnist_4channel/AB --name mnist_4channel_pix2pix_bs8 --model pix2pix --direction AtoB --input_nc 4 --output_nc 4
