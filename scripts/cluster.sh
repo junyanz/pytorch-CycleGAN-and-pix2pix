31fa871effa5adc522b61c888e4fe0ded792d40c
bossemel <18574762+bossemel@users.noreply.github.com>
2020-02-21 15:26:11 +0000

    added cluster commands

---
+++ scripts/cluster.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH --partition=standard
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-08:00:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(s1885912)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets/
+# Activate the relevant virtual environment:
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+cd ..
+sh /Users/leonie/Documents/MDS/MLP/GroupProject/pytorch-CycleGAN-and-pix2pix.nosync/scripts/pix2pix_mnist_4channel.sh
+
+#python train_evaluate_emnist_classification_system.py --filepath_to_arguments_json_file experiment_configs/emnist_tutorial_config.json
