70b4f03b76711162985d08f4ec055d75a06cba20
Clemens Macho <cmacho@stanford.edu>
2020-03-08 00:00:42 +0100

    modified train.py to allow saving multiple images per epoch

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
             if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                visualizer.display_current_results(model.get_current_visuals(), int(total_iters / opt.display_freq), save_result)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
