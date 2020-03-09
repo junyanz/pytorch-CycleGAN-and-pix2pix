8e01b74262be484e9acbc6f005240dd5c6db981a
bossemel <18574762+bossemel@users.noreply.github.com>
2020-03-09 12:54:16 +0000

    adjusted scripts for satellite data

---
+++ scripts/cluster_satellite.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH --partition=Teach-Standard
+#SBATCH --gres=gpu:2
+#SBATCH --mem=7500  # memory in Mb
+#SBATCH --time=0-08:00:00
+
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}datasets/
+
+# Activate the relevant virtual environment:
+rsync -ua /home/${STUDENT_ID}/pytorch-CycleGAN-and-pix2pix/tars/AB.tar.gz "${DATASET_DIR}"
+tar -xzf "${DATASET_DIR}/AB.tar.gz" -C "${DATASET_DIR}"
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+
+sh /home/s1885912/pytorch-CycleGAN-and-pix2pix/scripts/pix2pix_satellite.sh
+
+#python train_evaluate_emnist_classification_system.py --filepath_to_arguments_json_file experiment_configs/emnist_tutorial_config.json
---
+++ scripts/satellite_combine_A_and_B.sh
@@ -0,0 +1,2 @@
+set -ex
+python datasets/combine_A_and_B.py --fold_A datasets/satellite/\A --fold_B datasets/satellite/\B --fold_AB datasets/satellite/\AB
