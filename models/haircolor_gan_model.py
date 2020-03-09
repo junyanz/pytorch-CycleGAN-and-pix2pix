366c1d131d8608c7e98d19e74fa59ca85584f148
cmacho <58872556+cmacho@users.noreply.github.com>
2020-03-09 21:12:39 +0100

    use batch norm, change default options

--- models/haircolor_gan_model.py
+++ models/haircolor_gan_model.py
@@ -27,10 +27,12 @@ class HairColorGANModel(BaseModel):
         parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
         parser.set_defaults(dataset_mode='hair')
         parser.set_defaults(update_html_freq=400)
-        parser.set_defaults(print_freq=50)
+        parser.set_defaults(print_freq=40)
         parser.set_defaults(display_freq=400)
+        parser.set_defaults(norm='batch')
+        parser.set_defaults(batch_size=8)
         if is_train:
-            parser.add_argument('--lambda_cyc', type=float, default=10.0, help='weight for cycle loss')
+            parser.add_argument('--lambda_cyc', type=float, default=5.0, help='weight for cycle loss')
             parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
 
         return parser
