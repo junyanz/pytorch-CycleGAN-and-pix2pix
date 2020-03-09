92c5aaf0a3ef47c84737f0aab1b8f212e387f95c
bossemel <18574762+bossemel@users.noreply.github.com>
2020-03-09 13:20:54 +0000

    changed scripts

--- scripts/satellite_combine_A_and_B.sh
+++ scripts/satellite_combine_A_and_B.sh
@@ -1,2 +1,2 @@
 set -ex
-python datasets/combine_A_and_B.py --fold_A datasets/satellite/\A --fold_B datasets/satellite/\B --fold_AB datasets/satellite/\AB
+python datasets/combine_A_and_B.py --fold_A datasets/satellite_AB/\A --fold_B datasets/satellite_AB/\B --fold_AB datasets/satellite_AB/\AB
