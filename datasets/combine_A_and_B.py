a4a8002758b4fbdcab0c7702e284c22391e2bed5
CharlesDove <chadove98@gmail.com>
2020-06-09 13:57:19 -0400

    parallelize combine_A_and_B script

--- datasets/combine_A_and_B.py
+++ datasets/combine_A_and_B.py
@@ -2,6 +2,19 @@ import os
 import numpy as np
 import cv2
 import argparse
+from multiprocessing import Pool
+
+
+def image_write(path_A,path_B,path_AB):
+    im_A = cv2.imread(path_A, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+    im_B = cv2.imread(path_B, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+    im_AB = np.concatenate([im_A, im_B], 1)
+    cv2.imwrite(path_AB, im_AB)
+
+
+
+
+
 
 parser = argparse.ArgumentParser('create image pairs')
 parser.add_argument('--fold_A', dest='fold_A', help='input directory for image A', type=str, default='../dataset/50kshoes_edges')
@@ -15,8 +28,11 @@ for arg in vars(args):
     print('[%s] = ' % arg, getattr(args, arg))
 
 splits = os.listdir(args.fold_A)
+pool=Pool()
+print(splits)
 
 for sp in splits:
+#def f(sp):
     img_fold_A = os.path.join(args.fold_A, sp)
     img_fold_B = os.path.join(args.fold_B, sp)
     img_list = os.listdir(img_fold_A)
@@ -42,7 +58,10 @@ for sp in splits:
             if args.use_AB:
                 name_AB = name_AB.replace('_A.', '.')  # remove _A
             path_AB = os.path.join(img_fold_AB, name_AB)
-            im_A = cv2.imread(path_A, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
-            im_B = cv2.imread(path_B, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
-            im_AB = np.concatenate([im_A, im_B], 1)
-            cv2.imwrite(path_AB, im_AB)
+            pool.apply_async(image_write,args=(path_A,path_B,path_AB))
+            #im_A = cv2.imread(path_A, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+            #im_B = cv2.imread(path_B, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+            #im_AB = np.concatenate([im_A, im_B], 1)
+            #cv2.imwrite(path_AB, im_AB)
+pool.close()
+pool.join()
