41bc4ebaba2536226a4c2ab48b8b3a91a04cac7a
seovchinnikov <seovchinnikov@croc.ru>
2020-07-25 02:34:45 +0300

    Add ProGan model

--- models/networks.py
+++ models/networks.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
-
+from torch.nn import functional as F
+from math import sqrt
 
 ###############################################################################
 # Helper Functions
@@ -98,7 +99,7 @@ def init_weights(net, init_type='normal', init_gain=0.02):
     net.apply(init_func)  # apply the initialization function <init_func>
 
 
-def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
+def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[], init_weights_=True):
     """Initialize a network: 1. register CPU/GPU device (with multi-GPU support); 2. initialize the network weights
     Parameters:
         net (network)      -- the network to be initialized
@@ -111,11 +112,13 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     if len(gpu_ids) > 0:
         assert(torch.cuda.is_available())
         net.to(gpu_ids[0])
-    init_weights(net, init_type, init_gain=init_gain)
+    if init_weights_:
+        init_weights(net, init_type, init_gain=init_gain)
     return net
 
 
-def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[]):
+def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[],
+             init_weights=True):
     """Create a generator
 
     Parameters:
@@ -153,12 +156,15 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
         net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'unet_256':
         net = UnetGenerator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
+    elif netG == 'progan':
+        net = GeneratorProGan(input_code_dim=128)
     else:
         raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
-    return init_net(net, init_type, init_gain, gpu_ids)
+    return init_net(net, init_type, init_gain, gpu_ids, init_weights_=init_weights)
 
 
-def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal', init_gain=0.02, gpu_ids=[]):
+def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal', init_gain=0.02, gpu_ids=[],
+             init_weights=True):
     """Create a discriminator
 
     Parameters:
@@ -197,9 +203,11 @@ def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal'
         net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer)
     elif netD == 'pixel':     # classify if each pixel is real or fake
         net = PixelDiscriminator(input_nc, ndf, norm_layer=norm_layer)
+    elif netD == 'progan':
+        net = DiscriminatorProGan()
     else:
         raise NotImplementedError('Discriminator model name [%s] is not recognized' % netD)
-    return init_net(net, init_type, init_gain, gpu_ids)
+    return init_net(net, init_type, init_gain, gpu_ids, init_weights_=init_weights)
 
 
 ##############################################################################
@@ -612,3 +620,256 @@ class PixelDiscriminator(nn.Module):
     def forward(self, input):
         """Standard forward."""
         return self.net(input)
+
+
+class EqualLR:
+    def __init__(self, name):
+        self.name = name
+
+    def compute_weight(self, module):
+        weight = getattr(module, self.name + '_orig')
+        fan_in = weight.data.size(1) * weight.data[0][0].numel()
+
+        return weight * sqrt(2 / fan_in)
+
+    @staticmethod
+    def apply(module, name):
+        fn = EqualLR(name)
+
+        weight = getattr(module, name)
+        del module._parameters[name]
+        module.register_parameter(name + '_orig', nn.Parameter(weight.data))
+        module.register_forward_pre_hook(fn)
+
+        return fn
+
+    def __call__(self, module, input):
+        weight = self.compute_weight(module)
+        setattr(module, self.name, weight)
+
+
+def equal_lr(module, name='weight'):
+    EqualLR.apply(module, name)
+
+    return module
+
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input / torch.sqrt(torch.mean(input ** 2, dim=1, keepdim=True)
+                                  + 1e-8)
+
+
+class EqualConv2d(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+        conv = nn.Conv2d(*args, **kwargs)
+        conv.weight.data.normal_()
+        conv.bias.data.zero_()
+        self.conv = equal_lr(conv)
+
+    def forward(self, input):
+        return self.conv(input)
+
+
+class EqualConvTranspose2d(nn.Module):
+    ### additional module for OOGAN usage
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+        conv = nn.ConvTranspose2d(*args, **kwargs)
+        conv.weight.data.normal_()
+        conv.bias.data.zero_()
+        self.conv = equal_lr(conv)
+
+    def forward(self, input):
+        return self.conv(input)
+
+
+class EqualLinear(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        linear = nn.Linear(in_dim, out_dim)
+        linear.weight.data.normal_()
+        linear.bias.data.zero_()
+
+        self.linear = equal_lr(linear)
+
+    def forward(self, input):
+        return self.linear(input)
+
+
+class ConvBlockProGan(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, padding, kernel_size2=None, padding2=None,
+                 pixel_norm=True):
+        super().__init__()
+
+        pad1 = padding
+        pad2 = padding
+        if padding2 is not None:
+            pad2 = padding2
+
+        kernel1 = kernel_size
+        kernel2 = kernel_size
+        if kernel_size2 is not None:
+            kernel2 = kernel_size2
+
+        convs = [EqualConv2d(in_channel, out_channel, kernel1, padding=pad1)]
+        if pixel_norm:
+            convs.append(PixelNorm())
+        convs.append(nn.LeakyReLU(0.1))
+        convs.append(EqualConv2d(out_channel, out_channel, kernel2, padding=pad2))
+        if pixel_norm:
+            convs.append(PixelNorm())
+        convs.append(nn.LeakyReLU(0.1))
+
+        self.conv = nn.Sequential(*convs)
+
+    def forward(self, input):
+        out = self.conv(input)
+        return out
+
+
+def upscale(feat):
+    return F.interpolate(feat, scale_factor=2, mode='bilinear', align_corners=False)
+
+
+class GeneratorProGan(nn.Module):
+    def __init__(self, input_code_dim=128, in_channel=128, pixel_norm=True, tanh=True):
+        super().__init__()
+        self.input_dim = input_code_dim
+        self.tanh = tanh
+        # self.input_layer = nn.Sequential(
+        #     EqualConvTranspose2d(input_code_dim, in_channel, 4, 1, 0),
+        #     PixelNorm(),
+        #     nn.LeakyReLU(0.1))
+
+        self.input_layer = nn.Sequential(
+            EqualConv2d(input_code_dim, in_channel, 3, padding=1),
+            PixelNorm(),
+            nn.LeakyReLU(0.1))
+
+        self.progression_4 = ConvBlockProGan(in_channel, in_channel, 3, 1, pixel_norm=pixel_norm)
+        self.progression_8 = ConvBlockProGan(in_channel, in_channel, 3, 1, pixel_norm=pixel_norm)
+        self.progression_16 = ConvBlockProGan(in_channel, in_channel, 3, 1, pixel_norm=pixel_norm)
+        self.progression_32 = ConvBlockProGan(in_channel, in_channel, 3, 1, pixel_norm=pixel_norm)
+        self.progression_64 = ConvBlockProGan(in_channel, in_channel // 2, 3, 1, pixel_norm=pixel_norm)
+        self.progression_128 = ConvBlockProGan(in_channel // 2, in_channel // 4, 3, 1, pixel_norm=pixel_norm)
+        self.progression_256 = ConvBlockProGan(in_channel // 4, in_channel // 4, 3, 1, pixel_norm=pixel_norm)
+
+        self.to_rgb_8 = EqualConv2d(in_channel, 3, 1)
+        self.to_rgb_16 = EqualConv2d(in_channel, 3, 1)
+        self.to_rgb_32 = EqualConv2d(in_channel, 3, 1)
+        self.to_rgb_64 = EqualConv2d(in_channel // 2, 3, 1)
+        self.to_rgb_128 = EqualConv2d(in_channel // 4, 3, 1)
+        self.to_rgb_256 = EqualConv2d(in_channel // 4, 3, 1)
+
+        self.max_step = 6
+
+    def progress(self, feat, module):
+        out = F.interpolate(feat, scale_factor=2, mode='bilinear', align_corners=False)
+        out = module(out)
+        return out
+
+    def output(self, feat1, feat2, module1, module2, alpha):
+        if 0 <= alpha < 1:
+            skip_rgb = upscale(module1(feat1))
+            out = (1 - alpha) * skip_rgb + alpha * module2(feat2)
+        else:
+            out = module2(feat2)
+        if self.tanh:
+            return torch.tanh(out)
+        return out
+
+    def forward(self, input, step=0, alpha=-1):
+        if step > self.max_step:
+            step = self.max_step
+        # out_4 = self.input_layer(input.view(-1, self.input_dim, 1, 1))
+        out_4 = self.input_layer(input)
+        out_4 = self.progression_4(out_4)
+        out_8 = self.progress(out_4, self.progression_8)
+        if step == 1:
+            if self.tanh:
+                return torch.tanh(self.to_rgb_8(out_8))
+            return self.to_rgb_8(out_8)
+
+        out_16 = self.progress(out_8, self.progression_16)
+        if step == 2:
+            return self.output(out_8, out_16, self.to_rgb_8, self.to_rgb_16, alpha)
+
+        out_32 = self.progress(out_16, self.progression_32)
+        if step == 3:
+            return self.output(out_16, out_32, self.to_rgb_16, self.to_rgb_32, alpha)
+
+        out_64 = self.progress(out_32, self.progression_64)
+        if step == 4:
+            return self.output(out_32, out_64, self.to_rgb_32, self.to_rgb_64, alpha)
+
+        out_128 = self.progress(out_64, self.progression_128)
+        if step == 5:
+            return self.output(out_64, out_128, self.to_rgb_64, self.to_rgb_128, alpha)
+
+        out_256 = self.progress(out_128, self.progression_256)
+        if step == 6:
+            return self.output(out_128, out_256, self.to_rgb_128, self.to_rgb_256, alpha)
+
+
+class DiscriminatorProGan(nn.Module):
+    def __init__(self, feat_dim=128):
+        super().__init__()
+
+        self.progression = nn.ModuleList([ConvBlockProGan(feat_dim // 4, feat_dim // 4, 3, 1),
+                                          ConvBlockProGan(feat_dim // 4, feat_dim // 2, 3, 1),
+                                          ConvBlockProGan(feat_dim // 2, feat_dim, 3, 1),
+                                          ConvBlockProGan(feat_dim, feat_dim, 3, 1),
+                                          ConvBlockProGan(feat_dim, feat_dim, 3, 1),
+                                          ConvBlockProGan(feat_dim, feat_dim, 3, 1),
+                                          ConvBlockProGan(feat_dim + 1, feat_dim, 3, 1, 4, 0)])
+
+        self.from_rgb = nn.ModuleList([EqualConv2d(3, feat_dim // 4, 1),
+                                       EqualConv2d(3, feat_dim // 4, 1),
+                                       EqualConv2d(3, feat_dim // 2, 1),
+                                       EqualConv2d(3, feat_dim, 1),
+                                       EqualConv2d(3, feat_dim, 1),
+                                       EqualConv2d(3, feat_dim, 1),
+                                       EqualConv2d(3, feat_dim, 1)])
+
+        self.n_layer = len(self.progression)
+
+        self.linear = EqualLinear(feat_dim, 1)
+
+    def forward(self, input, step=0, alpha=-1):
+        for i in range(step, -1, -1):
+            index = self.n_layer - i - 1
+
+            if i == step:
+                out = self.from_rgb[index](input)
+
+            if i == 0:
+                out_std = torch.sqrt(out.var(0, unbiased=False) + 1e-8)
+                mean_std = out_std.mean()
+                mean_std = mean_std.expand(out.size(0), 1, 4, 4)
+                out = torch.cat([out, mean_std], 1)
+
+            out = self.progression[index](out)
+
+            if i > 0:
+                # out = F.avg_pool2d(out, 2)
+                out = F.interpolate(out, scale_factor=0.5, mode='bilinear', align_corners=False)
+
+                if i == step and 0 <= alpha < 1:
+                    # skip_rgb = F.avg_pool2d(input, 2)
+                    skip_rgb = F.interpolate(input, scale_factor=0.5, mode='bilinear', align_corners=False)
+                    skip_rgb = self.from_rgb[index + 1](skip_rgb)
+                    out = (1 - alpha) * skip_rgb + alpha * out
+
+        out = out.squeeze(2).squeeze(2)
+        # print(input.size(), out.size(), step)
+        out = self.linear(out)
+
+        return out
---
+++ models/progan_model.py
@@ -0,0 +1,162 @@
+import torch
+
+from .base_model import BaseModel
+from . import networks
+from torch.nn import functional as F
+try:
+    from apex import amp
+except ImportError:
+    print("Please install NVIDIA Apex for safe mixed precision if you want to use non default --opt_level")
+
+
+class ProGanModel(BaseModel):
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+
+        parser.set_defaults(netG='progan', netD='progan', dataset_mode='single', beta1=0.)
+
+        return parser
+
+    def accumulate(self, decay=0.999):
+        par1 = dict(self.netG.named_parameters())
+        par2 = dict(self.netC.named_parameters())
+
+        for k in par1.keys():
+            par1[k].data.mul_(decay).add_(1 - decay, par2[k].data)
+
+    def __init__(self, opt):
+
+        BaseModel.__init__(self, opt)
+        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
+        self.loss_names = ['G_GAN', 'D_real', 'D_fake']
+        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        self.visual_names = ['fake_B', 'real_B']
+        # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
+        if self.isTrain:
+            self.model_names = ['G', 'D', 'C']
+        else:  # during test time, only load G
+            self.model_names = ['G']
+        # define networks (both generator and discriminator)
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,
+                                      init_weights=False)
+        self.netC = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,
+                                      init_weights=False)
+
+        if self.isTrain:
+            self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
+                                          opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids,
+                                          init_weights=False)
+
+        assert opt.beta1 == 0
+        if self.isTrain:
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)
+            self.criterionL1 = torch.nn.L1Loss()
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_C = torch.optim.Adam(self.netC.parameters(), lr=opt.lr, betas=(opt.beta1, 0.99))
+            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.99))
+            self.optimizers.append(self.optimizer_C)
+            self.optimizers.append(self.optimizer_D)
+
+            if opt.apex:
+                [self.netC, self.netD], [self.optimizer_C, self.optimizer_D] = amp.initialize(
+                    [self.netC, self.netD], [self.optimizer_C, self.optimizer_D], opt_level=opt.opt_level, num_losses=2)
+
+        self.make_data_parallel()
+
+        # inner counters
+        self.total_steps = opt.n_epochs + opt.n_epochs_decay + 1
+        assert self.total_steps > 12
+        assert self.opt.crop_size % 2 ** 6 == 0
+        self.step = 1
+        self.iter = 0
+        self.alpha = 0.
+
+        # set fusing
+        self.netG.eval()
+        self.accumulate(0)
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+
+        The option 'direction' can be used to swap images in domain A and domain B.
+        """
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_B = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = F.interpolate(self.real_B, size=(4 * 2 ** self.step, 4 * 2 ** self.step), mode='bilinear')
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        batch_size = self.real_B.size(0)
+        z = torch.randn((batch_size, 128, self.opt.crop_size // (2 ** 6), self.opt.crop_size // (2 ** 6)), device=self.device)
+        self.fake_B = self.netC(z, step=self.step, alpha=self.alpha)
+
+    def backward_D(self):
+        """Calculate GAN loss for the discriminator"""
+        # Fake; stop backprop to the generator by detaching fake_B
+        fake_B = self.fake_B
+        pred_fake = self.netD(fake_B.detach(), step=self.step, alpha=self.alpha)
+        self.loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Real
+        real_B = self.real_B
+        pred_real = self.netD(real_B, step=self.step, alpha=self.alpha)
+        self.loss_D_real = self.criterionGAN(pred_real, True)
+        # combine loss and calculate gradients
+        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+
+        if self.opt.apex:
+            with amp.scale_loss(self.loss_D, self.optimizer_D, loss_id=0) as loss_D_scaled:
+                loss_D_scaled.backward()
+        else:
+            self.loss_D.backward()
+
+    def backward_G(self):
+        """Calculate GAN loss for the generator"""
+        # First, G(A) should fake the discriminator
+        fake_B = self.fake_B
+        pred_fake = self.netD(fake_B, step=self.step, alpha=self.alpha)
+        self.loss_G_GAN = self.criterionGAN(pred_fake, True)
+        self.loss_G = self.loss_G_GAN
+
+        if self.opt.apex:
+            with amp.scale_loss(self.loss_G, self.optimizer_C, loss_id=1) as loss_G_scaled:
+                loss_G_scaled.backward()
+        else:
+            self.loss_G.backward()
+
+    def optimize_parameters(self):
+        self.forward()  # compute fake images: G(A)
+        # update D
+        self.set_requires_grad(self.netD, True)  # enable backprop for D
+        self.optimizer_D.zero_grad()  # set D's gradients to zero
+        self.backward_D()  # calculate gradients for D
+        self.optimizer_D.step()  # update D's weights
+        # update generator C
+        self.set_requires_grad(self.netD, False)  # D requires no gradients when optimizing G
+        self.optimizer_C.zero_grad()  # set G's gradients to zero
+        self.backward_G()  # calculate graidents for G
+        self.optimizer_C.step()  # udpate G's weights
+        self.accumulate()  # fuse params
+
+    def update_inners_counters(self):
+        self.iter += 1
+        self.alpha = min(1, (2 / (self.total_steps // 6)) * self.iter)
+        if self.iter > self.total_steps // 6:
+            self.alpha = 0
+            self.iter = 0
+            self.step += 1
+
+            if self.step > 6:
+                self.alpha = 1
+                self.step = 6
+
+    def update_learning_rate(self):
+        super(ProGanModel, self).update_learning_rate()
+        self.update_inners_counters()
