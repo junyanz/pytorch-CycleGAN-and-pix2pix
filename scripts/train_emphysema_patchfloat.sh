0fd9d9e18ca4110c2e0ed3cad3f8576aeb6bda43
rohitrango <rohit.rango@gmail.com>
2021-02-17 14:07:31 -0500

    Added new scripts

---
+++ scripts/train_emphysema_patchfloat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#source activate venv
+source ~/.bashrc
+conda activate venv
+python train3d.py \
+    --dataroot /ocean/projects/asc170022p/rohit33 \
+    --dataset_mode copdpatchlabels \
+    --gpu_ids 0 \
+    --model cycle_gan_patch \
+    --patchfloat 1 \
+    --augment 1 \
+    --batch_size 500 \
+    --save_latest_freq 40000 \
+    --lambda_identity 0.05 \
+    --display_freq 40 \
+    --netG unet3d_patch_float \
+    --netD basic3d_patch_float \
+    --input_nc 1  \
+    --output_nc 1 \
+    --norm batch3d \
+    --subroot emphysemapatches \
+    --name copd_emphysema_float
