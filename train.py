15e227c4cb8db31053eeb41ef2b31feefadb9d18
ramit <ramit@nvidia.com>
2021-11-26 03:26:44 +0200

    LR update after epoch - for PT 1.10

--- train.py
+++ train.py
@@ -40,7 +40,6 @@ if __name__ == '__main__':
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
-        model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
@@ -69,6 +68,7 @@ if __name__ == '__main__':
                 model.save_networks(save_suffix)
 
             iter_data_time = time.time()
+        model.update_learning_rate()  # update learning rates *after* epoch (https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate)
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
             model.save_networks('latest')
