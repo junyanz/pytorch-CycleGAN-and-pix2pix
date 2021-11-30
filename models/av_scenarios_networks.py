d25e58fbcce9231fc5697e066c3c3e6244695004
ramit <ramit@nvidia.com>
2021-11-30 18:16:55 +0200

    up

--- models/av_scenarios_networks.py
+++ models/av_scenarios_networks.py
@@ -11,12 +11,19 @@ import functools
 
 class SceneGenerator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, opt):
         super().__init__()
+        self.map_enc = MapEncoder()
+        self.dim_latent_noise = opt.dim_latent_noise
+        self.batch_size = opt.batch_size
 
 
-    def forward(self, input):
+    def forward(self, in_map_feat):
         """Standard forward"""
+        map_latent = self.map_enc(in_map_feat)
+        latent_noise = torch.randn(self.batch_size, self.dim_latent_noise)
+        scene_latent = torch.concat(map_latent, latent_noise, dim=1)
+
         return self.model(input)
 
 
