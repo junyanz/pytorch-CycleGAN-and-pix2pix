1e355e70df9fd700ccf4c565b8119f5e0171150e
ramit <ramit@nvidia.com>
2021-12-07 16:09:14 +0200

    up

--- models/avsg_model.py
+++ models/avsg_model.py
@@ -188,8 +188,6 @@ class AvsgModel(BaseModel):
         """Run forward pass. This will be called by both functions <optimize_parameters> and <test>."""
         # generate the output of the generator given the input map
         self.fake_agents = self.netG(self.real_map)
-        # TODO: generator should trans the yaw to [0,2pi] ? check the range of the yaw in the data
-        pass
 
     #########################################################################################
 
@@ -218,15 +216,14 @@ class AvsgModel(BaseModel):
         #  the generator should fool the discriminator
         pred_fake = self.netD(self.real_map, self.fake_agents)
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
-
+        self.loss_G = self.loss_G_GAN
+        self.loss_G.backward()
         # # Second, we want G(map) = map, since the generator acts also as an encoder-decoder for the map
         # self.loss_G_L1 = self.criterionL1(self.reconstructed_map, self.real_map) * self.opt.lambda_L1
         # combine loss and calculate gradients
         # self.loss_G = self.loss_G_GAN + self.loss_G_L1
 
-        self.loss_G = self.loss_G_GAN
 
-        self.loss_G.backward()
 
     #########################################################################################
 
