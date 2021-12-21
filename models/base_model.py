8b46f5d1a12baa8cd9275a2e3ff2c95b59e6f8ae
ramit <Nor64536705>
2021-12-21 16:34:54 +0200

    save model bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -158,7 +158,8 @@ class BaseModel(ABC):
                 net = getattr(self, 'net' + name)
 
                 if len(self.gpu_ids) > 0 and torch.cuda.is_available():
-                    torch.save(net.module.cpu().state_dict(), save_path)
+                    torch.save(net.state_dict(), save_path)
+                    # torch.save(net.module.cpu().state_dict(), save_path)
                     net.cuda(self.gpu_ids[0])
                 else:
                     torch.save(net.cpu().state_dict(), save_path)
