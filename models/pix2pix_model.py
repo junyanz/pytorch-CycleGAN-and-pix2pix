2b0896f04ef7243b5961b574a9d480c06f083e2f
Ikko Ashimine <eltociear@gmail.com>
2022-12-01 03:52:10 +0900

    fix typo in pix2pix_model.py
    udpate -> update

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -124,4 +124,4 @@ class Pix2PixModel(BaseModel):
         self.set_requires_grad(self.netD, False)  # D requires no gradients when optimizing G
         self.optimizer_G.zero_grad()        # set G's gradients to zero
         self.backward_G()                   # calculate graidents for G
-        self.optimizer_G.step()             # udpate G's weights
+        self.optimizer_G.step()             # update G's weights
