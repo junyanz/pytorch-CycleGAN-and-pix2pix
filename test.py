8b3354e2fb8f71ca520b1e8a125015f1617889b8
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-04-19 00:05:15 +0200

    Update test.py
    Fixing uint16

--- test.py
+++ test.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
             save_path = os.path.join(web_dir, f'{image_name}_{label}.tiff')
             image_numpy = image_numpy.cpu().numpy()
             # Salva l'immagine come tiff utilizzando tifffile
-            tifffile.imwrite(save_path, image_numpy)         
+            tifffile.imwrite(save_path, image_numpy(np.uint16))         
             if opt.use_wandb:
                 wandb.save(save_path)    
             
