cbc4db84eb639975d8de5918be5dbbb3b2b53b2f
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-04-29 18:11:20 +0200

    Update base_options.py
    Changes

--- options/base_options.py
+++ options/base_options.py
@@ -38,7 +38,7 @@ class BaseOptions():
         parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
         parser.add_argument('--no_dropout', action='store_true', help='no dropout for the generator')
         # dataset parameters
-        parser.add_argument('--dataset_mode', type=str, default='mydataset', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization | mydataset]')
+        parser.add_argument('--dataset_mode', type=str, default='myaligned', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization | mydataset | myaligned]')
         parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
         parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
         parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
