3f7564d44cc12de934eeea33cee89fcf4f4dd458
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-04-30 13:10:38 +0200

    Update pix2pix_model.py

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -30,7 +30,7 @@ class Pix2PixModel(BaseModel):
         """
         # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
         #parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
-        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='myaligned')
+        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
