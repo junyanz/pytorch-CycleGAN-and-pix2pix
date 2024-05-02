575165615643ab17c8569a7685b2e2878a791b0f
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-05-02 12:52:22 +0200

    Update myaligned_dataset.py

--- data/myaligned_dataset.py
+++ data/myaligned_dataset.py
@@ -44,9 +44,9 @@ class MyAlignedDataset(BaseDataset):
         w, h = AB.shape[-1] // 2, AB.shape[-2] 
         A = Image.fromarray(AB[:, :w])
         B = Image.fromarray(AB[:, w:])
-        '''
-        A_array = np.array(A, dtype=np.int16)
-        B_array = np.array(B, dtype=np.int16)
+    
+        A_array = np.array(A)
+        B_array = np.array(B)
         
         if np.array_equal(A, B):
             print("Images A and B are equal.")
@@ -65,11 +65,11 @@ class MyAlignedDataset(BaseDataset):
         print("Type:", B_array.dtype)
         print("Min value:", np.min(B_array))
         print("Max value:", np.max(B_array),"\n")
-        '''
+        
         # apply the same transform to both A and B
         A = self.transform(A)
         B = self.transform(B)
-        '''        
+                
         if np.array_equal(A, B):
             print("Images A and B are equal after trasnform.")
 
@@ -87,7 +87,7 @@ class MyAlignedDataset(BaseDataset):
         print("Min value:", np.min(B_array_after))
         print("Max value:", np.max(B_array_after),"\n")
         
-        '''
+        
         return {'A': A, 'B': B, 'A_paths': AB_path, 'B_paths': AB_path}
 
     def __len__(self):
