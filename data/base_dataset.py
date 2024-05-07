03fb2b8d142111faca46fb75a875467ab9bfa5c6
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-05-07 20:11:08 +0200

    Update base_dataset.py

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -80,7 +80,7 @@ def get_params(opt, size):
 
 def get_transform(opt, params=None, grayscale=False, method=transforms.InterpolationMode.BICUBIC, convert=True):
     transform_list = []
-    if grayscale:
+    #if grayscale:
         #transform_list.append(transforms.Grayscale(1))
     if 'resize' in opt.preprocess:
         osize = [opt.load_size, opt.load_size]
