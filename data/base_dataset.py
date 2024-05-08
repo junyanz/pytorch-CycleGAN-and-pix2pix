4e323200ac2b8412fa013a7a331b97881bfe535a
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-05-08 18:53:19 +0200

    Update base_dataset.py

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -80,31 +80,31 @@ def get_params(opt, size):
 
 def get_transform(opt, params=None, grayscale=False): #method=transforms.InterpolationMode.BILINEAR, convert=True): #method=transforms.InterpolationMode.BICUBIC, convert=True):
     transform_list = []
-    if grayscale:
-        transform_list.append(transforms.Grayscale(1))
-    #if 'resize' in opt.preprocess:
-        #osize = [opt.load_size, opt.load_size]
-        #transform_list.append(transforms.Resize(osize, method))
-    #elif 'scale_width' in opt.preprocess:
-        #transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.load_size, opt.crop_size, method)))
+    #if grayscale:
+        #transform_list.append(transforms.Grayscale(1))
+    if 'resize' in opt.preprocess:
+        osize = [opt.load_size, opt.load_size]
+        transform_list.append(transforms.Resize(osize, method))
+    elif 'scale_width' in opt.preprocess:
+        transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.load_size, opt.crop_size, method)))
 
-    #if 'crop' in opt.preprocess:
-        #if params is None:
-            #transform_list.append(transforms.RandomCrop(opt.crop_size))
-        #else:
-            #transform_list.append(transforms.Lambda(lambda img: __crop(img, params['crop_pos'], opt.crop_size)))
+    if 'crop' in opt.preprocess:
+        if params is None:
+            transform_list.append(transforms.RandomCrop(opt.crop_size))
+        else:
+            transform_list.append(transforms.Lambda(lambda img: __crop(img, params['crop_pos'], opt.crop_size)))
 
-    #if opt.preprocess == 'none':
-        #transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
+    if opt.preprocess == 'none':
+        transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
 
-    #if not opt.no_flip:
-        #if params is None:
-            #transform_list.append(transforms.RandomHorizontalFlip())
-        #elif params['flip']:
-            #transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
+    if not opt.no_flip:
+        if params is None:
+            transform_list.append(transforms.RandomHorizontalFlip())
+        elif params['flip']:
+            transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
 
-    #if convert:
-        #transform_list += [transforms.ToTensor()]
+    if convert:
+        transform_list += [transforms.ToTensor()]
         #if grayscale:
             #transform_list += [transforms.Normalize((0.5,), (0.5,))]
         #else:
