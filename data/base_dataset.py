a6b5f03bf0729a8a72975724ba50241bebb600e7
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-05-13 17:53:46 +0200

    Update base_dataset.py

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -84,6 +84,7 @@ def get_transform(opt, params=None, grayscale=False, method=transforms.Interpola
         #transform_list += [transforms.ToTensor()]
         #transform_list.append(transforms.Grayscale(1))
     if 'resize' in opt.preprocess:
+        print("Vado qua")
         #osize = [opt.load_size, opt.load_size]
         #transform_list.append(transforms.Resize(osize, method))
     elif 'scale_width' in opt.preprocess:
