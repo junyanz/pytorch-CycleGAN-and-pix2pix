ffb38b00e8312b1c58d7a96ed30912810b635f84
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-05-14 23:56:56 +0200

    Update base_dataset.py

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -106,10 +106,10 @@ def get_transform(opt, params=None, grayscale=False, method=transforms.Interpola
 
     if convert:
         transform_list += [transforms.ToTensor()]
-        #if grayscale:
-            #transform_list += [transforms.Normalize((0.5,), (0.5,))]
-        #else:
-            #transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        if grayscale:
+            transform_list += [transforms.Normalize((0.5,), (0.5,))]
+        else:
+            transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     return transforms.Compose(transform_list)
 
 
