6a70dfd63ca310abc9679ec64bdae8c95aa3479d
Miguel Samaniego <ma.samaniego@hotmail.com>
2025-05-19 19:03:35 +0000

    Show git branch correctly

--- .devcontainer/update-bashrc.sh
+++ .devcontainer/update-bashrc.sh
@@ -10,6 +10,6 @@ if ! grep -q "parse_git_branch()" "$BASHRC"; then
         echo "parse_git_branch() {"
         echo "    git branch 2>/dev/null | sed -n '/\\* /s///p' | sed 's/.*/(\\0)/'"
         echo "}"
-        echo 'PS1="\\u \\[\\e[32m\\]\\w \\[\\e[91m\\]\\$(parse_git_branch)\\[\\e[00m\\]\\$ "'
+        echo 'PS1="\u \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]\$ "'
     } >> "$BASHRC"
 fi
